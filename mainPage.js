3b0d4479756a3a6f87cde62c46de3bf62a8a65f1
fares boutriga <boutrigafares@gmail.com>
2023-05-12 13:13:57 +0100

    hello1

--- mainPage.js
+++ mainPage.js
@@ -1,27 +1,28 @@
 
-//XO game
+//Rock game
 $(document).ready(function() {
     var parentDiv = $('<div class="flip-card" >'); // Create the parent div
     var secondDiv= $('<div class="flip-card-inner">')//create the inner side of the card
     var div3 = $('<div class="flip-card-front">'); // Create the front div
     var backDiv = $('<div class="flip-card-back">'); // Create the back-card div
+    
      // Create the button "get stard"  
     var button = $('<button class="button-card">');
     button.text("Get Started")
     // Create the image tag
     var imgTag = $('<img class="img-card"  >');
-    imgTag.attr('src', './photo.png');
+    imgTag.attr('src', './Rock Paper scissors/imgs/all.png');
     // Append the tags
     parentDiv.append(secondDiv);
     secondDiv.append(div3)
     secondDiv.append(backDiv)
-    backDiv.append(button)
+    // backDiv.append(button)
     div3.append(imgTag);
-
+    backDiv.append( "<a href='./Rock Paper scissors/Rock.html'> <button class='button-card'>Get Stard</button></a>")
     // Append the main div to the container div with cladd "card"
     $('.card').append(parentDiv);
   });
-  //Rock game
+  //Xo game
   
   $(document).ready(function() {
     var parentDiv = $('<div class="flip-card" >'); // Create the parent div
@@ -29,18 +30,18 @@ $(document).ready(function() {
     var div3 = $('<div class="flip-card-front">'); // Create the front div
     var backDiv = $('<div class="flip-card-back">'); // Create the back-card div
     // Create the button "get stard"  
-    var button = $('<button class="button-card">');
-    button.text("Get Started")
+    // var button = $('<button class="button-card">');
+    // button.text("Get Started")
     // Create the image tag
     var imgTag = $('<img class="img-card"  >');
-    imgTag.attr('src', 'https://images.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png');
+    imgTag.attr('src', './Xo.png');
     // Append the tags
     parentDiv.append(secondDiv);
     secondDiv.append(div3)
     secondDiv.append(backDiv)
     div3.append(imgTag)
-    backDiv.append(button)
-
+    // backDiv.append(button)
+     backDiv.append( "<a href='./XO/XO.html'> <button class='button-card'>Get Stard</button></a>")
     // Append the main div to the container div with cladd "card"
     $('.card').append(parentDiv);
   });
@@ -56,60 +57,20 @@ $(document).ready(function() {
     button.text("Get Started")
     // Create the image tag
     var imgTag = $('<img class="img-card"  >');
-    imgTag.attr('src', './photo.png');
+    imgTag.attr('src', './snac.png');
     // Append the tags
     parentDiv.append(secondDiv);
     secondDiv.append(div3)
     secondDiv.append(backDiv)
     div3.append(imgTag);
     backDiv.append(button)
-
+   
     // Append the main div to the container div with cladd "card"
     $('.card2').append(parentDiv);
   });
 
+  $(".pc-score").on("click",function(){
 
-
-
-
-
-
-//function Xo(plyaer1,player2){
-// // 	var game={}
-// // 	game.p1Chose=plyaer1,
-// // 	game.p2Chose=player2,
-// // 	game.case1=0
-// // 	game.case2=0
-// // 	game.case3=0
-// // 	game.case4=0
-// // 	game.case5=0
-// // 	game.case6=0
-// // 	game.case7=0
-// // 	game.case8=0
-// // 	game.case9=0
-// // 	game.result=res
-// // 	return game
-// // }
-
-// // var res=function(){
-// // if (this.case1===plyaer1&&)
-// // }function Xo(plyaer1,player2){
-// // 	var game={}
-// // 	game.p1Chose=plyaer1,
-// // 	game.p2Chose=player2,
-// // 	game.case1=0
-// // 	game.case2=0
-// // 	game.case3=0
-// // 	game.case4=0
-// // 	game.case5=0
-// // 	game.case6=0
-// // 	game.case7=0
-// // 	game.case8=0
-// // 	game.case9=0
-// // 	game.result=res
-// // 	return game
-// // }
-
-// // var res=function(){
-// // if ( this.case1===plyaer1&& this.case2===plyaer1&& this.case3===plyaer1)
-// // }   
+  }
+  )
+ 
